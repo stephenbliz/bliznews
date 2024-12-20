732d67b15d1f31bf64522cac81ef8ee070d2a996
bliz <uzomaebuka11@gmail.com>
2024-12-20 04:14:49 +0100

    finished half of the header

--- src/app/Component/header.tsx
+++ src/app/Component/header.tsx
@@ -1,33 +1,126 @@
+'use client';
+import { CSSProperties, useState } from "react";
 import { HiOutlineNewspaper } from "react-icons/hi2";
+import { RiMenu2Line } from "react-icons/ri";
+import { IoSearch } from "react-icons/io5";
+import { MdOutlineArrowDropDownCircle } from "react-icons/md";
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { TfiGoogle } from "react-icons/tfi";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaPinterest } from "react-icons/fa";
+import { RiTriangleFill } from "react-icons/ri";
+import Link from "next/link";
 
 export default function Header(){
+    const [search, setSearch] = useState('');
+    console.log(search);
+    const medias = [
+        {title: 'Follow us on Facebook', icon: <FaFacebookF />, colour: `#1773EA`, id: 1},
+        {title: 'Follow us on Twitter', icon: <FaTwitter />, colour: `#1C9CEA`, id: 2},
+        {title: 'Follow us on Google', icon: <TfiGoogle />, colour: '#D02C1F', id: 3},
+        {title: 'Follow us on LinkedIn', icon: <FaLinkedinIn />, colour: '#0073AE', id: 4},
+        {title: 'Follow us on Pinterest', icon: <FaPinterest />, colour: '#B6081A', id: 5}, 
+    ]
+
     return(
         <header
-            className="h-[10rem] border-b border-gray-300"
+            className="fixed top-0 left-0 w-[100vw] h-fit border-b border-gray-300"
         >
             <nav
-                className="h-[5rem] bg-red-600 px-[2rem] sm:px-[4rem] flex items-center justify-between"
+                className="h-[7rem] lg:h[5rem] bg-red-600 px-[1rem] text-white sm:px-[4rem] flex items-center justify-between"
             >
                 <div
                     className="flex items-center gap-3 w-[45%]"
                 >
-                    <div
-                        className="w-fit flex items-center gap-3"
+                    <Link
+                        href='/'
+                        className="w-fit flex items-center gap-2"
                     >
-                        <span><HiOutlineNewspaper /></span>
-                        <span>BLIZNEWS</span>
-                    </div>
+                        <span
+                            className="text-[3rem] rotate-[45deg]"
+                        >
+                            <HiOutlineNewspaper />
+                        </span>
+                        <span
+                            className="text-[1.5rem] spacing tracking-[.5rem] font-bold"
+                        >BLIZNEWS</span>
+                    </Link>
                     <div
-                        className="hidden items-center gap-1 lg:flex"
+                        className="hidden group py-6 relative items-center cursor-pointer gap-1 lg:flex capitalize"
                     >
-                        <span>follow us</span>
-                        <span>icon</span>
+                        <span>
+                            follow us
+                        </span>
+                        <span
+                            className="text-2xl"
+                        >
+                            <MdOutlineArrowDropDownCircle />
+                        </span>
+                        <div
+                            className=" absolute hidden group-hover:block top-[1rem] animate-slide-in"
+                        >
+                            <div
+                                className="absolute top-[3.5rem] left-0 border bg-white border-gray-300 "
+                            >
+                                {
+                                    medias.map((media)=>(
+                                        <Link
+                                            href='#'
+                                            className={`flex items-center gap-2 p-4 w-[22vw] mx-1 border-b border-gray-300  text-gray-400 hover:text-[var(--hover-colour)] transition duration-300 ease-linear`}
+                                            key={media.id}
+                                            style={{'--hover-colour': media.colour} as CSSProperties}
+                                        >
+                                            <span
+                                                className="w-fit"
+                                            >
+                                                {media.icon}
+                                            </span>
+                                            <span
+                                                className="w-fit"
+                                            >
+                                                {media.title}
+                                            </span>
+                                        </Link>
+                                    ))
+                                }
+                            </div>
+                            <div
+                                className="w-fit absolute text-xl left-[3rem] top-[2.4rem]"
+                            >
+                                <RiTriangleFill />
+                            </div>
+                        </div>
                     </div>
+                </div>
+                <div
+                    className="w-[45%] hidden lg:flex justify-end relative"
+                >
+                    <input 
+                        type="text" 
+                        name="search" 
+                        placeholder="Search"
+                        value={search}
+                        onChange={(e)=>setSearch(e.target.value)}
+                        className="h-[2rem] placeholder:text-gray-300 pl-2 pr-[2.5rem] py-2 rounded w-[60%] outline-none text-gray-600 border border-gray-300 focus:border-gray-400"
+                    />
+                    <span
+                        className="absolute right-2 top-1 text-2xl cursor-pointer text-gray-400 hover:text-gray-800 transition duration-300 ease-in-out"
+                    >
+                        <IoSearch />
+                    </span>
+                </div>
+                <div
+                    className="block lg:hidden w-fit text-center text-[3rem]"
+                >
+                    <RiMenu2Line
+                        // className="w-[100%]" 
+                    />
                 </div>
 
             </nav>
             <nav
-                className="h-[5rem] px-[2rem] sm:px-[4rem]"
+                className="h-[5rem] hidden lg:flex px-[4rem]"
             >
 
             </nav>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { transform } from "next/dist/build/swc/generated-native";
 import type { Config } from "tailwindcss";
 
 export default {
@@ -12,6 +13,15 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      animation: {
+        'slide-in': 'slideIn 0.3s ease-in-out'
+      },
+      keyframes: {
+        slideIn: {
+          '0%': { transform: 'translateY(2rem)', opacity : '0'},
+          '100%': { transform: 'translateY(0) ', opacity: '1'}
+        }
+      }
     },
   },
   plugins: [],
